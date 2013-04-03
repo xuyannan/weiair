1542343b161994e3ee0ceb20bfc3b40e4ca8d2f0
root <root@rst.(none)>
2013-04-03 08:27:13 +0000

    real reply

--- app.js
+++ app.js
@@ -7,17 +7,32 @@ var express = require('express')
   , routes = require('./routes');
 var url = require('url');
 var sha1 = require('sha1');
+var AqiApi = require('./chineseaqi.js');
 //var wechat = require('./wechat.js');
 var wechat = require('wechat');
+var ejs = require('ejs');
 var http = require('http');
 var connect = require('connect');
 var app = connect();
 app.use(connect.query());
 
+var api = new AqiApi('QfEJyi3oWKSBCnKrqp1v');
+
+var tpl = '<%=area%>的空气污染指数:<%=aqi%>\n\rpm2.5浓度:<%=pm2_5%>\n\r<%=quality%> \n\r<%=time_point%>';
+var compiled = ejs.compile(tpl);
+
 app.use('/', wechat('weiair', function (req, res, next) {
   // 微信输入信息都在req.weixin上
   var message = req.weixin;
-  res.reply('you just said: ' + message.Content);
+  var city = message.Content;
+  console.log('getting data for ' + message.Content);
+  api.getAvgPm25ForCity(message.Content, function(data){
+    res.reply(compiled(data));
+  },
+    function(data) {
+      res.reply('该城市还没有pm2.5数据，试试别的城市~');
+    }
+  );
 }));
 var port = 3000;
 http.createServer(app).listen(port);
--- chineseaqi.js
+++ chineseaqi.js
@@ -2,6 +2,13 @@ var jquery = require('jquery');
 
 var AqiApi = function(token) {
   this.token = token;
+  this.cities = undefined; 
+  /*
+  this.getSupportCities(function(data) {
+    this.cities = data.cities;
+    console.log(this.cities);
+  });
+  */
 };
 
 // 通用请求方法
@@ -11,16 +18,25 @@ var commonReqest = function(params) {
     function(obj) {
       if (obj.error) {
         console.log(obj.error);
+        if (params.errorCallback && typeof(params.errorCallback) == 'function') {
+            params.errorCallback(obj);
+        }
       }else if (params.callback && typeof(params.callback) == 'function') {
         params.callback(obj);
       };
     }
-  );}
+  );
+};
 
 AqiApi.prototype.init = function(token) {
   this.token = token;
-}
+};
 
+AqiApi.prototype.isSupported = function(city) {
+  var self = this;
+  console.log(self.cities);
+  return jquery.inArray(city, self.cities) >= 0;
+}
 
 // 得到所有有数据的城市
 AqiApi.prototype.getSupportCities = function(callback) {
@@ -35,32 +51,47 @@ AqiApi.prototype.getSupportCities = function(callback) {
 };
 
 // 获取某个城市的pm2.5数据
-AqiApi.prototype.getPm25ForCity = function(city, callback) {
+AqiApi.prototype.getPm25ForCity = function(city, callback, errorCallback) {
+  if (!city) {
+    return;
+  }
   var self = this;
   var url = 'http://pm25.in/api/querys/pm2_5.json';
   var data = {token: self.token, city: city};
   commonReqest({
     url: url,
     data: data,
-    callback: callback
+    callback: callback,
+    errorCallback: errorCallback
   });
 
 };
 
 // 获取一个城市最新的pm2.5平均值
-AqiApi.prototype.getAvgPm25ForCity = function(city, callback) {
+AqiApi.prototype.getAvgPm25ForCity = function(city, callback, errorCallback) {
+  if (!city) {
+    return;
+  }
   var self = this;
-  self.getPm25ForCity(city, function(data){
-    if (data && data.constructor.name == 'Array') {
-      callback(data[data.length - 1]);
-    } else {
-      console.log('getAvgPm25ForCity error');
+  self.getPm25ForCity(city, 
+    function(data){
+      if (data && data.constructor.name == 'Array') {
+        callback(data[data.length - 1]);
+      } else {
+        console.log('getAvgPm25ForCity error');
+      }
+    },
+    function(data) {
+      errorCallback(data);
     }
-  });
+  );
 };
 
 // 获取一个城市所有监测点的PM10数据
 AqiApi.prototype.getPm10ForCity = function(city, callback) {
+  if (!city) {
+    return;
+  }
   var self = this;
   var url = 'http://pm25.in/api/querys/pm10.json';
   var data = {token: self.token, city: city};
@@ -73,6 +104,9 @@ AqiApi.prototype.getPm10ForCity = function(city, callback) {
 
 // 获取一个城市最新的pm10平均值
 AqiApi.prototype.getAvgPm10ForCity = function(city, callback) {
+  if (!city) {
+    return;
+  }
   var self = this;
   self.getPm10ForCity(city, function(data){
     if (data && data.constructor.name == 'Array') {
