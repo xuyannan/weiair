8f1ff941d819becc8051525d350ace351ea33cfd
root <root@rst.(none)>
2013-04-03 00:43:38 +0000

    use wechat lib

--- app.js
+++ app.js
@@ -5,32 +5,20 @@
 
 var express = require('express')
   , routes = require('./routes');
+var url = require('url');
+var sha1 = require('sha1');
+//var wechat = require('./wechat.js');
+var wechat = require('wechat');
+var http = require('http');
+var connect = require('connect');
+var app = connect();
+app.use(connect.query());
 
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-});
+app.use('/', wechat('weiair', function (req, res, next) {
+  // 微信输入信息都在req.weixin上
+  var message = req.weixin;
+  res.reply('you just said: ' + message.Content);
+}));
+var port = 3000;
+http.createServer(app).listen(port);
+console.log("Express server listening on port %d", port);
